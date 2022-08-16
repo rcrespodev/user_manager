cc239522a2eebb1f3ffacacaa1f31a9f06190a70
rcrespodev <crespodev@gmail.com>
2022-08-16 12:13:45 -0300

    refactor: pkg cqrs. Aggregate id can be a key of source. The aggregate id is used for consumers to find the target aggregate.

--- pkg/kernel/cqrs/command/baseCommand.go
+++ pkg/kernel/cqrs/command/baseCommand.go
@@ -11,34 +11,9 @@ type Command interface {
 
 type BaseCommand struct {
 	commandUuid uuid.UUID
-	aggregateId string // key of aggregate created/updated/deleted.
-	commandId   Id     // examples of commandId: register_new_user, update_user, etc.
+	aggregateId AggregateId // key of aggregate created/updated/deleted.
+	commandId   Id          // examples of commandId: register_new_user, update_user, etc.
 	occurredOn  time.Time
-}
-
-func NewBaseCommand(aggregateId string, commandId Id) *BaseCommand {
-	return &BaseCommand{
-		commandUuid: uuid.New(),
-		aggregateId: aggregateId,
-		commandId:   commandId,
-		occurredOn:  time.Now(),
-	}
-}
-
-func (b BaseCommand) CommandUuid() uuid.UUID {
-	return b.commandUuid
-}
-
-func (b BaseCommand) AggregateId() string {
-	return b.aggregateId
-}
-
-func (b BaseCommand) CommandId() Id {
-	return b.commandId
-}
-
-func (b BaseCommand) OccurredOn() time.Time {
-	return b.occurredOn
 }
 
 type Id uint8
@@ -52,3 +27,30 @@ const (
 	UserLogged
 	UserLoggedOut
 )
+
+type AggregateId string
+
+func NewBaseCommand(aggregateId string, commandId Id) *BaseCommand {
+	return &BaseCommand{
+		commandUuid: uuid.New(),
+		aggregateId: AggregateId(aggregateId),
+		commandId:   commandId,
+		occurredOn:  time.Now(),
+	}
+}
+
+func (b BaseCommand) CommandUuid() uuid.UUID {
+	return b.commandUuid
+}
+
+func (b BaseCommand) AggregateId() AggregateId {
+	return b.aggregateId
+}
+
+func (b BaseCommand) CommandId() Id {
+	return b.commandId
+}
+
+func (b BaseCommand) OccurredOn() time.Time {
+	return b.occurredOn
+}
